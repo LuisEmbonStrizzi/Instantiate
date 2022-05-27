443c94a8b7325d4bea0ea514bddd5c80f7906f60
LuisEmbonStrizzi <luisembonstrizzi@gmail.com>
2022-05-27 10:18:00 -0300

    Initial commit

---
+++ Assets/CloneObjects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneObjects : MonoBehaviour
+{
+    public GameObject objectToClone;
+
+    public void InstantiateObject()
+    {
+        int i = 0;
+        while (i < 3)
+        {
+            Instantiate(objectToClone);
+            i++;
+        }
+        
+    }
+}
